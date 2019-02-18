bf4e0eead82c4a6f05a0edc5410ee2398f10c2da
Edward <edward.maesen@gmail.com>
2019-02-18 14:16:32 -0800

    Fix nav flicker on IE

--- client/src/App.vue
+++ client/src/App.vue
@@ -41,7 +41,7 @@
   src: local('Rosario'), local('Rosario-Regular'), url(/fonts/rosario.woff2) format('woff2');
   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
 }
-html, #nav, #nav a {
+html, #nav {
   background-color: #0c1c31;
   background-image: url(/img/bg.png);
   background-size: initial;
