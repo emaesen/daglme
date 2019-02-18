6113dbdee0bd1c9f61b68caec336f0efb7978009
Edward <edward.maesen@gmail.com>
2019-02-18 11:49:37 -0800

    Tweak content and styling

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -4,10 +4,10 @@
 
     <h2 class="center">A Call</h2>
     <div class="center outline">
-      <div class="poetic glow">
+      <div class="poetic glow margin-top5">
         Birthing of the New Earth Culture &amp; Consciousness
       </div>
-      <div class="h-color">
+      <div class="emph-color">
         <span class="emph">5 Minute Meditation, Each Day at 8 PM, Local Time</span>
         <br>
         (or choose first 5 minutes of any hour of the day)
@@ -20,8 +20,10 @@
       <li>Envision Earth and Sky as Already Illumined and Whole. Feel It!</li>
       <li>Release the Vision.</li>
       <li>Silently say: ‘This Meditation is for the Earth and Sky.’</li>
-      <li>Rest in the Infinite. Rest in the Light of Consciousness.<br>
-          Be empty by being open and receptive to the Higher Awareness.<br> 
+      <li>Rest in the Infinite.<br>
+          Rest in the Light of Consciousness.<br>
+          Be empty by being open.<br>
+          Be receptive to the Higher Awareness.<br> 
           ‘Thy Will Be Done.’<br>
           Listen Within.</li>
     </ul>
@@ -39,22 +41,21 @@
 
     <h2>Global Awakening</h2>
     <ul>
-      <li>Join us,</li>
-      <li class="indent">in a daily wave of Awareness Awakening propagating around the globe.</li>
-      <li>The power of group meditation:</li>
-      <li class="indent">the group effect amplifies the intentions of each individual.</li>
-      <li>Share on social media:</li>
-      <li class="indent">Use tag <span class="emph">#DailyGlobalMeditation</span> or <span class="emph">#daglme</span></li>
+      <li>A daily wave of Awareness Awakening</li>
+      <li>propagating around the globe.</li>
+      <li class="margin-top5">A group effect</li>
+      <li>amplifying the intentions of each individual.</li>
+      <li class="margin-top5">#DailyGlobalMeditation, #daglme</li>
     </ul>
 
     <h2>Presented and Supported by:</h2>
     <ul>
-      <li><a href="http://EarthCareGlobalTV.com">EarthCare Global TV</a></li>
-      <li><a href="https://InSharedOneness.com">In Shared Oneness retreats</a>:<br>
+      <li><a href="http://EarthCareGlobalTV.com" class="external">EarthCare Global TV</a></li>
+      <li class="margin-top5"><a href="https://InSharedOneness.com" class="external">In Shared Oneness retreats</a>:<br>
         <ul>
-        <li><a href="http://MarySaintMarie.com">Mary Saint Marie</a></li>
-        <li><a href="http://letempledelesprit.com/">Angelique Jan Pera</a></li>
-        <li><a href="https://kagalakan.com">Edward Maesen</a></li>
+        <li><a href="http://MarySaintMarie.com" class="external">Mary Saint Marie</a></li>
+        <li><a href="http://letempledelesprit.com/" class="external">Angelique Jan Pera</a></li>
+        <li><a href="https://kagalakan.com" class="external">Edward Maesen</a></li>
         </ul>
       </li>
     </ul>
