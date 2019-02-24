f95569c486d02367140bdf558c519ea9af7513f6
Edward <edward.maesen@gmail.com>
2019-02-24 14:44:34 -0800

    Tweak content after review

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,43 +1,56 @@
 <template>
   <div class="about view">
-
     <h1>About the Daily Global Meditation</h1>
 
     <p class="margin-top20">
-      The Daily Global Meditation is a call for peoples around the world to participate in a short daily meditation or prayer at 8PM local time.<br>
+      The Daily Global Meditation is a call for peoples around the world to participate in a short daily meditation or prayer at 8PM local time.
+      <br>
       If 8PM is an inconvenient time, one may choose any other hour of the day and align with individuals for which that hour is their local 8PM.
     </p>
     <p>
-      The goal is to have enough daily participants to reach a critical mass that will produce improvements in the quality of life for mankind. 
+      The goal is to have enough daily participants to reach a critical mass that will produce improvements in the quality of life for mankind.
     </p>
     <p>
-      Research in 1976 found that when 1% of a community practiced Transcendental Meditation, the crime rate was reduced by 16% on average. 
-      This is known as the <a href="https://tmhome.com/benefits/study-maharishi-effect-group-meditation-crime-rate/" target="_blank">"Maharishi effect"</a>.
+      Research in 1976 found that when 1% of a community practiced Transcendental Meditation, the crime rate was reduced by 16% on average.
+      This is known as the
+      <a
+        href="https://tmhome.com/benefits/study-maharishi-effect-group-meditation-crime-rate/"
+        target="_blank"
+      >"Maharishi effect"</a>.
     </p>
     <p>
       One doesn't need to be an experienced meditator to join our Daily Global Meditation. We suggest a focused attention daily for 5 minutes at 8PM local time, to envision the Earth and Sky as Illumined and Whole.
+    </p>
+    <p>
+      Daily Global Meditation embraces all ways, paths and practices.
     </p>
 
     <div class="poetic compact normal margin-top30">
       <div>
         A daily meditation for the Earth and the Sky.
       </div>
-      <div class="margin-top5">
-        A daily prayer for peace, love, equality and abundance…
+      <div class="margin-top10">
+        A daily meditation for Peace, Love, Equality and Wisdom.
       </div>
       <div class="margin-top5">
-        Weaving a web of light around the globe…
+        A daily meditation for Simplicity, Patience and Abundance…
       </div>
       <div class="margin-top5">
-        Expanding day by day, driven by the power of group meditation.
+        Weaving a web of Light around the globe…
       </div>
       <div class="margin-top5">
-        A catalyst for all that are on the brink of awakening.
+        Expanding day by day,
       </div>
       <div class="margin-top5">
-        A beacon for lost souls.
+        Driven by the power of group meditation.
+      </div>
+      <div class="margin-top10">
+        A catalyst for The Awakening…
       </div>
       <div class="margin-top5">
+        And a beacon for Souls.
+      </div>
+      <div class="margin-top10">
         A daily anchor to feel United as One.
       </div>
     </div>
@@ -47,13 +60,17 @@
     <h2 class="h1">About the Presenters</h2>
 
     <p class="margin-top20">
-    The Daily Global Meditation is presented by
-    <router-link to="/about/mary-saint-marie">Mary Saint-Marie</router-link>,
-    <router-link to="/about/angelique-jan-pera">Angelique Jan Pera</router-link> and
-    <router-link to="/about/edward-maesen">Edward Maesen</router-link>,
-    in association with <router-link to="/about/earthcare-global-tv">EarthCare Global TV</router-link> 
-    and <router-link to="/about/in-shared-oneness">In Shared Oneness</router-link>.<br>
-    <span class="instruction">(Click any of these names to view their information below)</span>
+      The Daily Global Meditation is presented by
+      <router-link to="/about/mary-saint-marie">Mary Saint-Marie</router-link>,
+      <router-link to="/about/angelique-jan-pera">Angelique Jan Pera</router-link> 
+      and
+      <router-link to="/about/edward-maesen">Edward Maesen</router-link>,
+      in association with
+      <router-link to="/about/earthcare-global-tv">EarthCare Global TV</router-link>
+      and
+      <router-link to="/about/in-shared-oneness">In Shared Oneness</router-link>.
+      <br>
+      <span class="instruction">(Click any of these names to view their information below)</span>
     </p>
 
     <transition name="view-fade" mode="out-in">
--- client/src/views/presenters/InSharedOneness.vue
+++ client/src/views/presenters/InSharedOneness.vue
@@ -5,14 +5,23 @@
       In Shared Oneness offers customized transformational spiritual retreats in the activating beauty of Mt Shasta, northern California, USA.
     </p>
     <p>
-      Individuals, couples, families and groups are assisted in discovering their fullest Unique Self Expression beyond limiting beliefs and fearful thoughts.
+      Individuals, couples, families and groups are assisted in discovering their fullest Unique Self Expression beyond limiting beliefs, fearful thoughts and mistaken identity.
     </p>
     <p>
-      In Shared Oneness is a unique union of three experienced facilitators who are Consciousness educators, coaches and way-showers. 
-      With combined well over a century experience in consciousness explorations, <router-link to="/about/angelique-jan-pera#info">Angelique Jan Pera</router-link>, <router-link to="/about/mary-saint-marie#info">Mary Saint-Marie</router-link> and <router-link to="/about/edward-maesen#info">Edward Maesen</router-link> hold Sacred Space for your Wholeness.
+      In Shared Oneness is a unique union of three experienced facilitators who are Consciousness educators and way-showers. 
+      With combined well over a century experience in consciousness explorations, 
+      <router-link to="/about/angelique-jan-pera#info">Angelique Jan Pera</router-link>, 
+      <router-link to="/about/mary-saint-marie#info">Mary Saint-Marie</router-link> 
+      and 
+      <router-link to="/about/edward-maesen#info">Edward Maesen</router-link> 
+      hold Sacred Space for your Wholeness.
     </p>
-    <div class="poetic">Recognize, Experience and Embrace the Power and Beauty of <span class="nowrap">Your Uniqueness.</span></div>
-    <div>Web site: <a href="https://InSharedOneness.com" class="external">In Shared Oneness retreats</a></div>
+    <div class="poetic">
+      Recognize, Experience and Embrace the Power and Beauty of <span class="nowrap">Your Uniqueness.</span>
+    </div>
+    <div>
+      Web site: <a href="https://InSharedOneness.com" class="external">In Shared Oneness retreats</a>
+    </div>
 
   </div>
 </template>
