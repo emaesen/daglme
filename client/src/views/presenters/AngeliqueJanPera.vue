527639d1d4bc6e774e8f29cd14e8381f719c595d
Edward <edward.maesen@gmail.com>
2019-02-27 15:29:19 -0800

    Add bio

--- client/src/views/presenters/AngeliqueJanPera.vue
+++ client/src/views/presenters/AngeliqueJanPera.vue
@@ -7,15 +7,53 @@
     </div>
 
     <div class="background">
-      Angelique is a licensed psycho-spiritual therapist in San Francisco and Mount Shasta. She has been assisting individuals, couples, families and groups to express their Unique Passionate Self as an aspect of the Whole. She has been practicing in private sessions, workshops and retreats in the USA, Europe and Mexico since 1979.
+      Angelique is a psycho-spiritual therapist in Mount Shasta and San Francisco, licensed as a Marriage, Family, Child Therapist.<br>
+      <br>
+      She has assisted individuals, couples, families and groups to express their Unique Passionate Self as an aspect of the Whole. She has been practicing in private sessions, workshops and retreats in the USA, Europe and Mexico since 1979.<br>
+      <br>
+      Her life long strong belief is that if we truly realized we are All, we could do NO harm to any beings, including ourselves.
     </div>
 
-    <!--
-    <h3>...</h3>
+    <h3>The Early Years</h3>
     <div>
-      ...
+      From about the age of three, Angelique was able to feel, see and know things that others seemed not to notice.<br>
+      <br>
+      She would tune into her mother's friends during their visits, observing their body language, listening to the quality of their voices more than their words, paying deep attention to each one.  When she felt something that seemed out of balance she would instinctively touch them where she could sense something was problematic.  When she felt their emotional pain she would give them a hug, or stroke their hair.  To her this was normal, “sharing love”.<br>
+      <br>
+      Angelique was aware of seeking an actual conscious relationship with God/the Divine/All that Is, when she was about the age of five.  Unable to find others like her she tried to become mainstream as her childhood unfolded.<br>
+      <br>
+      At age 18 Angelique had a life-altering experience that plunged her into deep depression.  It took her back on the path of seeking. Her mother was very “trans-personal” and exposed her to many different avenues of spiritual training, courses and retreats. Angelique began to find her way during her college years. 
     </div>
-    -->
+
+    <h3>Psychotherapy Study</h3>
+    <div>
+      A couple years after college, Angelique decided to go back to school and get her degree and license as a psychotherapist. She planned her course work centered around psycho-analysis, so she could get a deep understanding of the psychological theories offered at the time.<br>
+      <br>
+      Throughout her college years Angelique worked with people to rid themselves of limiting beliefs, thought forms, and mistaken identities that kept them from being their Unique Selves. Helping them use their gifts, talents and abilities to maximize their creative Self Expression.
+    </div>
+
+    <h3>Assisting Individuals, Couples, Families, Communities</h3>
+    <div>
+      Angelique's multi-dimensional work is considered unconventional, transcending traditional and mainstream ideas. She uses psychotherapy, hypnotic/trance-inducing techniques, and other mind-expanding techniques, meditation, visualization, and forgiveness processes for individuals to reconnect with their Unique Identity, their unique expression of the Undivided Whole.<br>
+      <br>
+      Her dedication is to the understanding that “we are all One with All that Is”, and she views this as foundational to our way back to Wholeness and Wellbeing; Individually and Collectively.<br>
+      <br>
+      Along with this is her firm conviction that our differences provide diversity, and are meant to be celebrated, nurtured and shared with one another. It is the substance from which our depth and breadth of Love can be cultivated. Whether it is to find your sacred partner, or your life's purpose. 
+    </div>
+
+    <h3>Assisting Animals</h3>
+    <div>
+      Angelique had her dog Luis as part of her practice for 18 years. He helped, open, sooth and calm hundreds of her clientele.<br>
+      <br>
+      Inspired by “we are all One with All that Is”, Angelique's work now includes healing work with animals, along with their guardians.<br>
+      <br>
+      She helps people heal their ailing animals, and connect them even more deeply with one another. The healing power of dogs, and animals in general, is widely known. ALL beings are important, and have their purpose in the lives of their loved ones.
+    </div>
+
+
+    <div class="poetic margin-top20">
+      To Know the <span class="nowrap">‘Wholeness within the Oneness’</span> is our <span class="nowrap">Natural State of Being</span>.
+    </div>
 
     <div class="contact margin-top50">
       Angelique<br>
