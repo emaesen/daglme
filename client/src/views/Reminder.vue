4ec7d45b0c318b4e27d80b1ceb2e1354db078a38
Edward <edward.maesen@gmail.com>
2019-03-20 14:20:00 -0700

    Work in progress

--- client/src/views/Reminder.vue
+++ client/src/views/Reminder.vue
@@ -2,27 +2,79 @@
   <div class="reminder view">
     <h1>Daily Global Meditation Reminder</h1>
 
-    <p class="margin-top20">
-      <transition name="fade" mode="out-in">
-        <div key="notify-enable" v-if="!isNotificationEnabled">
-          If you'd like to receive a daily reminder at 8 PM (or at any other hour of your choice) you may 
-          <span class="action" @click="enableNotifications">enable daily notifications</span>. Your browser will ask to grant permission to display notifications<br>
-          (You can easily disable again at any time later if so desired)
-        </div>
-        <div key="notify-disable" v-if="isNotificationEnabled && !isNotificationGranted">
-          You have requested a daily meditation reminder. Please grant notification permission in your browser: it should show a popup with option to allow or block notifications.
-        </div>
-        <div key="notify-disable" v-if="isNotificationGranted">
-          You have enabled a daily meditation reminder. You may
-          <span class="action" @click="disableNotifications">disable daily notifications here</span> or you can directly revoke notification permissions in your browser (typically by clicking an information icon in the browser's URL bar).
-        </div>
-      </transition>
-    </p>
+    <div class="margin-top20">
+      <div>
+        If you'd like to receive a daily meditation reminder, you may enable daily reminder notifications.<br>
+        By default the reminder will show at 8 o'clock in the evening (8 PM or 20:00 hour), but you may set any other time.<br>
+        You can disable the reminder at any time if so desired.<br>
+      </div>
+      <div class="margin-top10">
+        If you'd like to setup the daily meditation reminder, please follow the steps below:
+      </div>
+    </div>
 
-    <h2>Privacy notice</h2>
-    <div>
-        No data is exchanged with outside parties.<br>
-        Notifications are a direct communication between your local copy of this web page and the device that you view this on.
+    <h2>1. Enable a Daily Global Meditation Reminder</h2>
+    <transition name="fade" mode="out-in">
+      <div key="s1a" v-if="!isNotificationEnabled">
+        <div>
+          Click the button to enable a Daily Global Meditation Reminder.<br>
+          <button class="action" @click="enableNotifications">Enable a daily reminder</button> at <input type="time" v-model="reminderTime" step="900"/> (<span class="mono">{{ reminderTime }}</span>)
+        </div>
+        <div class="note">
+          This action will request your browser for permission to display notifications.
+        </div>
+      </div>
+      <div key="s1b" v-else>
+        You have enabled the daily meditation reminder already. Wonderful!
+      </div>
+    </transition>
+
+    <h2>2. Grant Permission for Notifications</h2>
+    <transition name="fade" mode="out-in">
+      <div key="s2a" v-if="!isNotificationEnabled">
+        You'll be asked to grant permission once you have enabled the reminder in step 1...
+      </div>
+      <div key="s2c" v-else>
+        <div v-if="!isNotificationGranted">
+          <span class="emph-alert">Please grant notification permission in your browser:</span> it should show a popup with option to allow or block notifications.
+          <div class="note" v-if="isNotificationDenied">
+            Current notification permission: "<span :class="{'emph-alert':isNotificationDenied}">{{ notificationPermission }}</span>".
+            <span class="emph-alert">You may need to allow notifications in your device's settings.</span>
+          </div>
+        </div>
+        <div v-else>
+          You have granted permission already. Wonderful!
+        </div>
+      </div>
+    </transition>
+
+    <h2>3. Manage your Daily Global Meditation Reminder</h2>
+    <transition name="fade" mode="out-in">
+      <div key="s3a" v-if="!isNotificationGranted">
+        You'll be able to manage your Daily Global Meditation Reminder here once you have granted permission in step 2...
+      </div>
+      <div key="s3b" v-else>
+        <div>
+          Change your reminder time:
+          <input type="time" v-model="reminderTime" step="900"/> (<span class="mono">{{ reminderTime }}</span>)
+        </div>
+        <div class="emph margin-top10">
+          or
+        </div>
+        <div>
+          <button class="action" @click="disableNotifications">Disable your daily reminder</button>
+        </div>
+        <div class="note">
+          You may disable daily notifications here or you can directly revoke notification permissions in your browser (typically by clicking an information icon in the browser's URL bar) or device (typically in your settings menu).
+        </div>
+      </div>
+    </transition>
+
+    <h3>Privacy Notice</h3>
+    <div class="notice smallfont">
+      Reminder notifications are a direct communication between your local copy of this web page and the device that you view this on.<br>
+      No data is exchanged with outside parties.<br>
+      You may opt out entirely by blocking "notifications" in your browser or device’s settings.
     </div>
 
   </div>
@@ -36,7 +88,7 @@ export default {
     return {
       isNotificationSupported: false,
       isNotificationEnabled: false,
-      isNotificationGranted: false
+      reminderTime: "20:00"
     }
   },
   mounted() {
@@ -48,7 +100,18 @@ export default {
     }
   },
   computed: {
-
+    timeAmPm() {
+      return this.reminderTime;
+    },
+    notificationPermission() {
+      return Notification.permission;
+    },
+    isNotificationGranted() {
+      return Notification.permission==="granted";
+    },
+    isNotificationDenied() {
+      return Notification.permission==="denied";
+    }
   },
   methods: {
     spawnNotification(body, title) {
