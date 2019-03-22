826aba604ca2b1f7739f81e5ba5294e92aff5ca7
Edward <edward.maesen@gmail.com>
2019-03-22 11:59:15 -0700

    Add service worker import file

--- client/vue.config.js
+++ client/vue.config.js
@@ -17,6 +17,7 @@ module.exports = {
     //https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin
     workboxOptions: {
       importWorkboxFrom: "local",
+      importScripts: ["sw-notifications.js"],
       navigateFallback: "/index.html",
       navigateFallbackBlacklist: [/img\/media/],
       // our app is simple and non-critical and all code resources are versioned so 
