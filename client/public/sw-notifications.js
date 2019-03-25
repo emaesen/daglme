def849f8c306e4548e47c6fa9f63c8c382d58317
Edward <edward.maesen@gmail.com>
2019-03-25 13:00:19 -0700

    Try message channel

--- client/public/sw-notifications.js
+++ client/public/sw-notifications.js
@@ -2,10 +2,12 @@
 console.log("[sw] in sw-notifications");
 
 self.addEventListener('message', function(event) {
-  console.log("[sw] Message received ", event);
+  console.log("[sw] Message received: ", event);
+  event.ports[0].postMessage("Reply from [sw]: message received");
+  event.ports[0].start();
 });
 
 
 self.addEventListener('push', function(event) {
-  console.log("[sw] Push Notification received ", event);
+  console.log("[sw] Push Notification received: ", event);
 });
--- client/src/utils/notification.js
+++ client/src/utils/notification.js
@@ -1,12 +1,33 @@
 function send_message_to_sw(msg) {
-  if ('serviceWorker' in navigator) {
-    console.log("[u/n] checking if service worker is active...");
-    navigator.serviceWorker.ready
-      .then(function(swreg) {
-        console.log("[u/n] sending message to service worker...");
-        swreg.active.postMessage(msg);
-      });
-  }
+  // thanks to http://craig-russell.co.uk/2016/01/29/service-worker-messaging.html
+  return new Promise(function(resolve, reject){
+    // Create a Message Channel
+    var msg_chan = new MessageChannel();
+
+    // Handler for recieving message reply from service worker
+    msg_chan.port1.onmessage = function(event){
+      console.log("[u/n] return message received ", event);
+      if(event.data.error){
+        reject(event.data.error);
+      }else{
+        resolve(event.data);
+      }
+    };
+
+    if ('serviceWorker' in navigator) {
+      console.log("[u/n] checking if service worker is active...");
+      navigator.serviceWorker.ready
+        .then(function(swreg) {
+          console.log("[u/n] sending message to service worker... accepting reply...");
+          swreg.active.postMessage(msg, [msg_chan.port2]);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    }
+  
+  });
+
 }
 
 export default send_message_to_sw;
