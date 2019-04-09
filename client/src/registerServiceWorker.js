a064006015df38b3f7910b08262eb14651c54115
Edward <edward.maesen@gmail.com>
2019-04-09 10:59:14 -0700

    Post status messages

--- client/src/registerServiceWorker.js
+++ client/src/registerServiceWorker.js
@@ -2,49 +2,54 @@
 
 import { register } from 'register-service-worker'
 
+function postMsg(msg, swreg) {
+  if (swreg && swreg.active) {
+    swreg.active.postMessage({msg: msg});
+  } else {
+    self.navigator.serviceWorker.ready.then(function(reg) {
+      reg.active.postMessage({msg: msg});
+      // reg.clients.matchAll().then(function (clients){
+      //   clients.forEach(function(client){
+      //     console.log("client", client);
+      //     client.postMessage({
+      //       msg: "sw-updated"
+      //     });
+      //   });
+      // });
+    })  
+  }
+}
+
+
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready (swreg) {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB', swreg
-      );
-      swreg.active.postMessage({msg: "sw:ready"});
+      console.log('[rsw] App is served from cache.');
+      postMsg("sw:ready", swreg)
     },
     registered (swreg) {
-      console.log('Service worker has been registered.', swreg)
+      console.log('[rsw] Service worker has been registered.')
+      postMsg("sw:registered", swreg)
     },
     cached (swreg) {
-      console.log('Content has been cached for offline use.', swreg)
+      console.log('[rsw] Content has been cached for offline use.')
+      postMsg("sw:cached", swreg)
     },
     updatefound (swreg) {
-      console.log('New content is downloading.', swreg)
+      console.log('[rsw] New content is downloading.')
+      postMsg("sw:updatefound", swreg)
     },
     updated (swreg) {
-      console.log('New content is available; please refresh.', {swreg, self})
-      if (swreg.active) {
-        console.log({swregActive: swreg.active});
-        swreg.active.postMessage({msg: "sw:updated"});
-      } else {
-        self.navigator.serviceWorker.ready.then(function(reg) {
-          console.log({reg});
-          reg.active.postMessage({msg: "sw:updated"});
-          // reg.clients.matchAll().then(function (clients){
-          //   clients.forEach(function(client){
-          //     console.log("client", client);
-          //     client.postMessage({
-          //       msg: "sw-updated"
-          //     });
-          //   });
-          // });
-        })  
-      }
+      console.log('[rsw] New content is available; please refresh.')
+      postMsg("sw:updated", swreg)
     },
     offline () {
-      console.log('No internet connection found. App is running in offline mode.')
+      console.log('[rsw] No internet connection found. App is running in offline mode.')
+      postMsg("sw:offline")
     },
     error (error) {
-      console.error('Error during service worker registration:', error)
+      console.error('[rsw] Error during service worker registration:', error)
+      postMsg("sw:error:" + error);
     }
   })
 }
