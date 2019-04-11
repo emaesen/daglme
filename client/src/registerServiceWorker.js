f6618df63feba556b306d770f00c14081cba8c38
Edward <edward.maesen@gmail.com>
2019-04-10 17:41:19 -0700

    Add reload logic

--- client/src/registerServiceWorker.js
+++ client/src/registerServiceWorker.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-console */
+// executes in the client app
 
 import { register } from 'register-service-worker'
 
 function postMsg(msg, swreg) {
   if (swreg && swreg.active) {
-    swreg.active.postMessage({msg: msg});
+    swreg.active.postMessage({msg});
   } else {
     self.navigator.serviceWorker.ready.then(function(reg) {
-      reg.active.postMessage({msg: msg});
+      reg.active.postMessage({msg});
     })  
   }
 }
@@ -27,16 +28,26 @@ if (process.env.NODE_ENV === 'production') {
       postMsg("sw:cached", swreg)
     },
     updatefound (swreg) {
-      console.log('[rsw] New content is downloading.')
+      console.log('[rsw] New content is downloading.', {swreg})
       postMsg("sw:updatefound", swreg)
     },
     updated (swreg) {
-      console.log('[rsw] New content is available; please refresh.')
+      console.log('[rsw] New content is available; please refresh.', {swreg})
       // need to instruct user to close/reopen tab,
       // -or- invoke skipWaiting(), preferably from client,
       // to activate updated service worker:
       // https://developers.google.com/web/tools/workbox/guides/advanced-recipes#offer_a_page_reload_for_users
       // https://redfin.engineering/how-to-fix-the-refresh-button-when-using-service-workers-a8e27af6df68
+      let refreshing;
+      self.navigator.serviceWorker.addEventListener( 'controllerchange',
+        () => {
+          if ( refreshing ) {
+            return;
+          }
+          refreshing = true;
+          window.location.reload();
+        }
+      );
       postMsg("sw:updated", swreg)
     },
     offline () {
