bdc9e1c1501cc038bc083a55e3e8fb0ef63e6f31
Edward <edward.maesen@gmail.com>
2019-04-12 09:47:21 -0700

    Move notification logic to util file

--- client/public/sw-customconfig.js
+++ client/public/sw-customconfig.js
@@ -1,14 +1,5 @@
 console.log("[sw-c] in sw-customconfig");
 
-let isClockStartPending = false;
-let timerID;
-let isNotificationGranted = false;
-let reminderTime = null;
-let reminderHour = null;
-let reminderMinute = null;
-
-
-
 self.addEventListener('activate', function (event) {
   console.log('[sw-c] SW Activate event received', event);
   event.waitUntil(
@@ -44,11 +35,6 @@ self.addEventListener('message', function(event) {
     new Promise(function(resolve) {
       if (event.data.action) {
         switch (event.data.action) {
-          case "setNotificationParams":
-            setNotificationParams(event.data.payload);
-            postReturnMsg("Reminder settings updated");
-            resolve();
-            break;
           case "spawnNotification":
             spawnNotification(event.data.payload).then(() => {
               postReturnMsg("Notification spawned");
@@ -56,6 +42,7 @@ self.addEventListener('message', function(event) {
             });
             break;
           case "skipWaiting":
+            // alternative means to invoke skipWaiting
             self.skipWaiting().then(() => {
               postReturnMsg("skipWaiting invoked");
               resolve();
@@ -142,63 +129,3 @@ function spawnNotification(opts /*body, duration, title, doVibrate*/) {
   });
 }
 
-
-function startClock() {
-  stopClock();
-  if(isNotificationGranted && !isClockStartPending) {
-    // clock ticks once every minute
-    let multiplier = 60;
-    let delay = 60 - new Date().getSeconds();
-    console.log("[sw-c] Initialize the clock - start in " + delay + " seconds");
-    setTimeout(() => {
-      timerID = setInterval(ticktock, multiplier * 1000);
-      console.log("[sw-c] Start the clock (" + timerID + ")");
-      ticktock();
-      isClockStartPending = false;
-    }, delay * 1000);
-    isClockStartPending = true;
-  }
-}
-
-function stopClock() {
-  if (timerID) {
-    console.log("[sw-c] Stop the clock (" + timerID + ")");
-    clearInterval(timerID);
-    timerID = null;
-  }
-}
-
-function ticktock() {
-  var now = new Date();
-  var hours = now.getHours();
-  var minutes = now.getMinutes();
-  console.log("[sw-c] tick tock (" + timerID + ") " + hours + ":" + minutes + " (" + reminderTime + ")");
-  if (hours === 0 && minutes === 0) {
-    // restart/recalibrate the clock
-    console.log("[sw-c] Recalibrate the clock");
-    startClock();
-  }
-  if (isNotificationGranted && 
-      hours === reminderHour && minutes === reminderMinute) {
-    spawnNotification({
-      body:"For 5 minutes, envision the Earth and Sky as Illumined and Whole...\nThanks for your participation!"
-    });
-    console.log("[sw-c] Your Daily Global Meditation Reminder");
-  }
-}
-
-function setNotificationParams(params) {
-  console.log("[sw-c] Update notification params", params, {isNotificationGranted_old: isNotificationGranted});
-  if (isNotificationGranted===false && params.isNotificationGranted===true) {
-    // use 0 delta to ensure isNotificationGranted is set
-    setTimeout(startClock,0);
-  }
-  if (isNotificationGranted===true && params.isNotificationGranted===false) {
-    // use 0 delta to ensure isNotificationGranted is set
-    setTimeout(stopClock,0);
-  }
-  isNotificationGranted = params.isNotificationGranted;
-  reminderTime = params.reminderTime;
-  reminderHour = params.reminderHour;
-  reminderMinute = params.reminderMinute;
-}
---
+++ client/src/utils/reminder.js
@@ -0,0 +1,87 @@
+import {
+  isServiceWorkerSupported,
+  send_message_to_sw
+  } from "./sw-interface.js";
+
+let isClockStartPending = false;
+let timerID;
+let isNotificationGranted = false;
+let reminderTime = null;
+let reminderHour = null;
+let reminderMinute = null;
+
+export const isNotificationSupported = "Notification" in window;
+export const areNotificationsAvailable = isServiceWorkerSupported 
+  && isNotificationSupported 
+  && Notification.permission !== "denied";
+
+function startClock() {
+  stopClock();
+  if(isNotificationGranted && !isClockStartPending) {
+    // clock ticks once every minute
+    let multiplier = 60;
+    let delay = 60 - new Date().getSeconds();
+    console.log("[u/rem] Initialize the clock - start in " + delay + " seconds");
+    setTimeout(() => {
+      timerID = setInterval(ticktock, multiplier * 1000);
+      console.log("[u/rem] Start the clock (" + timerID + ")");
+      ticktock();
+      isClockStartPending = false;
+    }, delay * 1000);
+    isClockStartPending = true;
+  }
+}
+
+function stopClock() {
+  if (timerID) {
+    console.log("[u/rem] Stop the clock (" + timerID + ")");
+    clearInterval(timerID);
+    timerID = null;
+  }
+}
+
+function ticktock() {
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  console.log("[u/rem] tick tock (" + timerID + ") " + hours + ":" + minutes + " (" + reminderTime + ")");
+  if (hours === 0 && minutes === 0) {
+    // restart/recalibrate the clock
+    console.log("[u/rem] Recalibrate the clock");
+    startClock();
+  }
+  if (isNotificationGranted && 
+      hours === reminderHour && minutes === reminderMinute) {
+    spawnNotification({
+      body:"For 5 minutes, envision the Earth and Sky as Illumined and Whole...\nThanks for your participation!"
+    });
+    console.log("[u/rem] Your Daily Global Meditation Reminder");
+  }
+}
+
+export function spawnNotification(payload) {
+  console.log("[u/rem] spawn notification", {payload});
+  return send_message_to_sw({
+    action:"spawnNotification", 
+    payload:payload
+  });
+}
+
+export function setNotificationParams(params) {
+  return new Promise(function(resolve) {
+    console.log("[u/rem] Update notification params", params);
+    if (isNotificationGranted===false && params.isNotificationGranted===true) {
+      // use 0 delta to ensure isNotificationGranted is set
+      setTimeout(startClock,0);
+    }
+    if (isNotificationGranted===true && params.isNotificationGranted===false) {
+      // use 0 delta to ensure isNotificationGranted is set
+      setTimeout(stopClock,0);
+    }
+    isNotificationGranted = params.isNotificationGranted;
+    reminderTime = params.reminderTime;
+    reminderHour = params.reminderHour;
+    reminderMinute = params.reminderMinute;
+    resolve("Reminder settings updated");
+  })
+}
