003a6a84953035e43f4bfb180e3c0816c848e1d7
Edward <edward.maesen@gmail.com>
2019-04-24 10:10:55 -0700

    Initialize vuex store for reminders

--- client/src/store.js
+++ client/src/store.js
@@ -5,12 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    clockTime: "--:--",
+    reminderTime: "20:00",
+    isNotificationEnabled: false
+  },
+  getters: {
+    clockTime: state => state.clockTime,
+    reminderTime: state => state.reminderTime,
+    isNotificationEnabled: state => state.isNotificationEnabled
   },
   mutations: {
-
+    // can be used directly for synchronous events
+    SET_CLOCK_DISPLAY: (state, clockTime) => (state.clockTime = clockTime),
+    SET_REMINDER_TIME: (state, reminderTime) => (state.reminderTime = reminderTime),
+    SET_IS_NOTIFICATION_ENABLED: (state, isNotificationEnabled) => (state.isNotificationEnabled = isNotificationEnabled)
   },
   actions: {
-
+    // for async functionality - commit a mutation
+    // e.g. setTimeTick: (context, tick) => context.commit("SET_TIME_TICK", tick)
   }
 })
