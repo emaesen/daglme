f4df7826b0a7c0dcc2c5820c8a22f71ba3b5cddc
Edward <edward.maesen@gmail.com>
2019-05-16 11:49:47 -0700

    Tweak content

--- client/src/views/presenters/InSharedOneness.vue
+++ client/src/views/presenters/InSharedOneness.vue
@@ -8,13 +8,13 @@
       Individuals, couples, families and groups are assisted in discovering their fullest Unique Self Expression beyond limiting beliefs, fearful thoughts and mistaken identity.
     </p>
     <p>
-      In Shared Oneness is a unique union of three experienced facilitators who are Consciousness educators and way-showers. 
+      In Shared Oneness is a union of three experienced facilitators who are Consciousness educators and way-showers.<br>
       With combined well over a century experience in consciousness explorations, 
       <router-link to="/presenters/angelique-jan-pera">Angelique Jan Pera</router-link>, 
       <router-link to="/presenters/mary-saint-marie">Mary Saint-Marie</router-link> 
       and 
       <router-link to="/presenters/edward-maesen">Edward Maesen</router-link> 
-      hold Sacred Space for your Wholeness.
+      draw each from their own unique life experiences of conscious awakening to assist and guide individuals in <i>their</i> awakening to a realization of harmony and well-being.
     </p>
     <div class="poetic">
       Recognize, Experience and Embrace the Power and Beauty of <span class="nowrap">Your Uniqueness.</span>
